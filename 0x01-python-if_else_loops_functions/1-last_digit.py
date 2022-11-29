bc8dfb51fd31e4c3fd0a9725109b741825800c56
Godswill Enaohwo <godswilleo@yahoo.com>
2022-11-29 17:14:18 +0000

    1 last digit update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,11 @@ last_number = number_str[-1]
 l_num_int = int(last_number)
 
 if (l_num_int > 5):
-    print("Last digit of ", number, " is ", l_num_int, "and is greater than 5")
+    print("Last digit of", number, "is", l_num_int, "and is greater than 5")
 
 if (l_num_int == 0):
-    print("Last digit of ", number, " is ", l_num_int, " and is 0")
+    print("Last digit of", number, "is", l_num_int, "and is 0")
 
 if (l_num_int < 6 and l_num_int != 0):
-    last_line = " and is less than 6 and not 0"
-    print("Last digit of ", number, " is ", l_num_int, last_line)
+    last_line = "and is less than 6 and not 0"
+    print("Last digit of", number, "is", l_num_int, last_line)
