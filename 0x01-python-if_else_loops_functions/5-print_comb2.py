e70de9e713d2925437c129321f6e57e92c8e2773
Godswill Enaohwo <godswilleo@yahoo.com>
2022-11-29 19:26:27 +0000

    5 print comb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 
 for num in range(100):
-    if (num < 10):
-        insert = 0
-
-    else:
-        insert = ""
 
     if (num != 99):
-        print(f"{insert}{num}, ", end="")
+        print(f"{num:02}", end=", ")
 
     else:
         print(f"{num}")
