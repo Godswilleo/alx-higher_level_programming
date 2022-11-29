da4026988e20baaf4be8c2ed5cf5669453fa89b7
Godswill Enaohwo <godswilleo@yahoo.com>
2022-11-29 18:26:27 +0000

    print alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,5 +2,4 @@
 
 for alphabet in range(97, 123):
     if chr(alphabet) != 'q' and chr(alphabet) != 'e'
-	   print("{}".format(chr(alphabet)), end="")
-
+    print("{}".format(chr(alphabet)), end="")
