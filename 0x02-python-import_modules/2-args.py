3d9a60d89b6976ebdb674330170d92784663e478
Godswill Enaohwo <godswilleo@yahoo.com>
2022-12-01 10:08:39 +0000

    2-args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+arg_num = len(sys.argv) - 1
+
+if arg_num == 0:
+    print("0 arguments.")
+
+elif arg_num == 1:
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+
+else:
+    for i in range(arg_num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
