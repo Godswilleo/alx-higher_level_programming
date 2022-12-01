b90611296a7d53008b4044fdfffeedfb68a986a2
Godswill Enaohwo <godswilleo@yahoo.com>
2022-12-01 15:13:39 +0000

    easy print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-arg_num = len(sys.argv) - 1
+    arg_num = len(sys.argv) - 1
 
-if arg_num == 0:
-    print("0 arguments.")
+    if arg_num == 0:
+        print("0 arguments.")
 
-elif arg_num == 1:
-    print("1 argument:")
-    print("1: {}".format(sys.argv[1]))
+    elif arg_num == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
 
-else:
-    for i in range(arg_num):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    else:
+        for i in range(arg_num):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
