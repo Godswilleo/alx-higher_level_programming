b852c608e5739d66c41138dfe6adfd1c04945f1b
Godswill Enaohwo <godswilleo@yahoo.com>
2022-12-05 19:32:36 +0000

    new lit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,9 @@
 
 
 def new_in_list(my_list, idx, element):
-    p = my_list.copy()
-    p[idx] = element
-    return(p)
+    if(idx < 0 or idx >= len(my_list)):
+	    return(my_list)
+    else:
+        p = my_list.copy()
+        p[idx] = element
+        return(p)
