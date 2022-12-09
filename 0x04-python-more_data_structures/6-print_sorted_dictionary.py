decfdbeebfa5b3e6f5f590686e85a677e932ed09
Godswill Enaohwo <godswilleo@yahoo.com>
2022-12-09 09:42:13 +0000

    sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    for x,y in sorted(a_dictionary.items()):
-        print('{}: {}'.format(x,y))
+    for x, y in sorted(a_dictionary.items()):
+        print('{}: {}'.format(x, y))
