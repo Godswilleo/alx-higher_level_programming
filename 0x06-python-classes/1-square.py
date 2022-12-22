daeaa29712b8f314bd782821f43ce16bd28fa010
Godswill Enaohwo <godswilleo@yahoo.com>
2022-12-22 08:26:03 +0000

    1 square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 0-square.py by Enaohwo Godswill
 """The class defines a square and also define a
-private instance attribute; size"""
+private instance attribute size"""
 
 
 class Square:
