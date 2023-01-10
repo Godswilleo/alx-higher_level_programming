b60dadc6fdeeb14a9c6b3fea88440f8fbb27723d
Godswill Enaohwo <godswilleo@yahoo.com>
2023-01-10 19:09:06 +0000

    rmeove loop to 0-read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+""" Module to demonstrate reading form a txt file"""
 
 def read_file(filename=""):
+    """ Function demonstrating reading from a txt file"""
     with open(filename,'r', encoding='utf-8') as f:
         print(f.read(), end="")
 	    
