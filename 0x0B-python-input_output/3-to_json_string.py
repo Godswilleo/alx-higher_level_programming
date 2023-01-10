e45e1df32f25b3c92f235916f4c18b1dd7cc733e
Godswill Enaohwo <godswilleo@yahoo.com>
2023-01-10 19:36:57 +0000

    json representation of object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" demonstrates json representation of an object """
+import json
+
+def to_json_string(my_obj):
+    """ demonstrates the json representation of an object """
+    return json.dumps(my_obj)    
