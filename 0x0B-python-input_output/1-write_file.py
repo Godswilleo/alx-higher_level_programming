d12af8dcd8454c714046f2b82834920271d28b8a
Godswill Enaohwo <godswilleo@yahoo.com>
2023-01-10 19:21:13 +0000

    writing to a file with erase

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,3 +6,9 @@ def write_file(filename="", text=""):
     """ function demonstrates writing to a txt file """
     with open(filename, mode="w", encoding="utf-8") as f:
         f.write(text)
+        
+        charNum = 0
+        
+        for char in text:
+            charNum += 1
+    return charNum
